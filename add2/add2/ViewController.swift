fc457e674a0d5a84a1ddd969aa00bab90aa00af7
cuijianhang <a5759@mac25.local>
2016-03-24 12:04:57 +0800

    +-x/
    +-x/

--- add2/add2/ViewController.swift
+++ add2/add2/ViewController.swift
@@ -29,6 +29,7 @@ class ViewController: UIViewController {
     }
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -41,4 +42,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
